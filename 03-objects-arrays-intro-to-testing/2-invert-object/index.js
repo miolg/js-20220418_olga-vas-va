d62fddd5b6017e346880511205ab23844477fa95
Olga Vasileva <ovasileva@rdv-it.ru>
2022-04-30 23:43:11 +0300

    Added function invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj && Object.keys(obj)) {
+    return Object.fromEntries(Object.entries(obj).map(([item1, item2]) => [item2, item1]));
+  }
 }
