b04f0f67285c91203ebe237503e03213f6e3309a
Olga Vasileva <ovasileva@rdv-it.ru>
2022-05-01 14:05:21 +0300

    Added function trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let currentSymbolCount = 1;
+  let prevSymbol;
 
+  function checkSymbols(result, current) {
+    if (!prevSymbol) {
+      prevSymbol = result;
+    }
+
+    if (prevSymbol !== current) {
+      prevSymbol = current;
+      currentSymbolCount = 1;
+      return result + current;
+    }
+
+    currentSymbolCount++;
+    if (currentSymbolCount > size) {
+      return result;
+    }
+
+    return result + current;
+  }
+
+  return ((size !== 0) && string && [...string].reduce((prev, current) => checkSymbols(prev, current))) || '';
 }
