d4aa6efd86c0feee88045e3f9f64323b2ac3c205
Olga Vasileva <ovasileva@rdv-it.ru>
2022-05-24 01:17:35 +0300

    Added Tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,63 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    const tooltipWrapper = document.createElement(`div`);
+    tooltipWrapper.innerHTML = `<div class="tooltip"></div>`;
+    this.element = tooltipWrapper.firstElementChild;
+
+    Tooltip.instance = this;
+  }
+
+  initialize () {
+    document.addEventListener('pointerover', this.show.bind(this));
+    document.addEventListener('pointerout', this.hide.bind(this));
+    document.addEventListener('mousemove', this.onMouseMove.bind(this));
+  }
+
+  show({ target } = event) {
+    let tooltipText = target.dataset.tooltip;
+    if (!tooltipText) {
+      return;
+    }
+    this.render(tooltipText);
+    
+  }
+
+  render(text) {
+    this.element.textContent = text;
+    document.body.append(this.element);
+  }
+
+  hide({ target } = event) {
+    let tooltipText = target.dataset.tooltip;
+    if (!tooltipText) {
+      return;
+    }
+
+    this.element.textContent = '';
+    this.element.remove();
+  }
+
+  onMouseMove({ target, offsetX, offsetY } = event) {
+    let tooltipText = target.dataset.tooltip;
+    if (!tooltipText) {
+      return;
+    }
+    const tooltipOffset = 15;
+    this.element.style.left = `${offsetX + tooltipOffset}px`;
+    this.element.style.top = `${offsetY + tooltipOffset}px`;
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.showTooltip);
+    document.removeEventListener('pointerout', this.hideTooltip);
+    document.removeEventListener('mousemove', this.onMouseMove);
+    this.element.remove();
   }
 }
 
